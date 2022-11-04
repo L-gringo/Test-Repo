14565e48efc9120b9e8bf030eab9dbc7f592a707
ALIOU Mahmoulh <mahmoulhaliou@yahoo.fr>
2022-11-04 16:11:13 +0100

    Remove some code

--- script.py
+++ script.py
@@ -1,16 +1,4 @@
-import math
-import os
-import sys
-
 import requests
-
-
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://yahoo.com")
